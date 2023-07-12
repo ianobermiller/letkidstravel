324e91667b99169e3f1f4fdedcea4bb2aedb82a5
Ian Obermiller <ian@obermillers.com>
2023-07-12 18:10:33 -0400

    Hero image

--- templates/Index.tsx
+++ templates/Index.tsx
@@ -9,15 +9,13 @@ interface Props {
 export function Index({ posts }: Props) {
   return (
     <Layout title="Let Kids Travel">
-      <main>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.path}>
-              <a href={post.slug}>{post.title}</a>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <ul className="post-list">
+        {posts.map((post) => (
+          <li key={post.path}>
+            <a href={post.slug}>{post.title}</a>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
