1c0b2ec1a5a6c1872392d77ba78e285ca71cb5de
Ian Obermiller <ian@obermillers.com>
2023-07-13 09:47:05 -0400

    Use modern jsx transform

--- templates/Index.tsx
+++ templates/Index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Layout } from "./Layout.jsx";
 import { PostData } from "../types.js";
+import { Layout } from "./Layout.jsx";
 
 interface Props {
   posts: Array<PostData>;
