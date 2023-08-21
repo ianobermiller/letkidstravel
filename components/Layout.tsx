3c33ae925ec6642330486e03c5fa7b198265dbee
Ian Obermiller <ian@obermillers.com>
2023-08-21 08:16:40 -0400

    Add search by google

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,11 +22,31 @@ export function Layout({ children, title }: Props) {
               <a href='/'>Let Kids Travel</a>
             </h1>
             <aside className='my-2 flex flex-wrap content-center gap-2 '>
-              <a
-                className='rounded-full bg-slate-100 px-2 py-2 leading-none'
-                href='https://instagram.com/letkidstravel'>
-                <FaSearch title='Search' />
-              </a>
+              <div className='relative h-8 w-8'>
+                <form
+                  action='http://www.google.com/search'
+                  method='get'
+                  target='_blank'>
+                  <input
+                    className='absolute right-0 h-8 w-full rounded-full bg-slate-100 px-2 py-2 leading-none outline-offset-1 outline-teal-400 transition-all duration-200 ease-in-out focus:w-64 focus:outline-2'
+                    id='search'
+                    name='q'
+                    placeholder='Search'
+                    type='text'
+                  />
+                  <label
+                    className='absolute left-0 top-0 rounded-full bg-slate-100 px-2 py-2 leading-none'
+                    htmlFor='search'>
+                    <FaSearch title='Search' />
+                  </label>
+                  <input
+                    name='q'
+                    type='hidden'
+                    value='site:letkidstravel.com'
+                  />
+                </form>
+              </div>
+
               <a
                 className='rounded-full bg-slate-100 px-2 py-2 leading-none'
                 href='https://instagram.com/letkidstravel'>
